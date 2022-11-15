a19a03e84b87d681ceb8010cf2d5aeac2e18f3cb
Jefferson <calleromero.jefferson@gmail.com>
2022-11-15 18:27:06 -0300

    Se agrego una navBar y se instalo bootstrap

---
+++ src/components/nav_bar/navBar.js
@@ -0,0 +1,14 @@
+import './navBar.css';
+
+const NavBar = () => {
+	return (
+		<nav>
+			<h1>TITULO</h1>
+			<button>boton1</button>
+			<button>boton2</button>
+			<button>boton2</button>
+		</nav>
+	)
+};
+
+export default NavBar;
